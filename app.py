acd2abef2e1c304ec49ddea5c604b9c2955f298f
Gopal Kedia <kedia.gopal10@gmail.com>
2022-11-22 12:43:31 +0530

    initial commit

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 import glob
 import re
 import numpy as np
-import torch
+# import torch
 from PIL import Image
-import albumentations as aug
-from efficientnet_pytorch import EfficientNet
+# import albumentations as aug
+# from efficientnet_pytorch import EfficientNet
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
@@ -18,21 +18,40 @@ from gevent.pywsgi import WSGIServer
 # Define a flask app
 app = Flask(__name__)
 
-model = torch.load("best_model.pth")
-model.eval()
+# model = torch.load("best_model.pth")
+# model.eval()
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+model=load_model('CropDoc.h5')
+
 def model_predict(file, model):
-    image = Image.open(file)
-    image = np.array(image)
-    transforms = aug.Compose([
-            aug.Resize(256,256),
-            aug.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225),max_pixel_value=255.0,always_apply=True),
-            ])
-    image = transforms(image=image)["image"]
-    image = np.transpose(image, (2, 0, 1)).astype(np.float32)
-    image = torch.tensor([image], dtype=torch.float)
-    preds = model(image)
-    preds = np.argmax(preds.detach())
+    my_image=Image.open(file)
+    my_img_arr=np.array(my_image)
+    my_img_arr=np.expand_dims(my_img_arr,axis=0)
+    if my_img_arr.max()>1:
+        preds=model.predict(my_img_arr/256)
+    else:
+        preds=model.predict(my_img_arr)
+
+    preds=np.argmax(preds)
     return preds
+
+
+# def model_predict(file, model):
+#     image = Image.open(file)
+#     image = np.array(image)
+#     transforms = aug.Compose([
+#             aug.Resize(256,256),
+#             aug.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225),max_pixel_value=255.0,always_apply=True),
+#             ])
+#     image = transforms(image=image)["image"]
+#     image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+#     image = torch.tensor([image], dtype=torch.float)
+#     preds = model(image)
+#     preds = np.argmax(preds.detach())
+#     return preds
 
 
 @app.route('/')
@@ -51,10 +70,12 @@ def index1():
 def upload():
     # Get the file from post request
     f = request.files['file']
-    cat = np.load("cat.npy")
+    cat = np.load("cat.npy", allow_pickle=True)
     # Make prediction
+        # with open(filename, 'rb') as file:
+        #     binaryData = file.read()
     preds = model_predict(f, model)
-    result = cat[preds]            
+    result = cat[preds]
     return result
 
 
